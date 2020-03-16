b79986a5c4ef1df8c0044137931c82ee19286df5
bartosz.ciesielczyk <bartosz.ciesielczyk@meelogic.com>
2020-03-16 21:30:35 +0100

    brancz injt BARCIE

---
+++ BARCIE/BARCIE_interface.h
@@ -0,0 +1,16 @@
+/**
+ * Responsible: Bart Ciesielczyk
+ *
+ *
+ * History log:
+ * 2020.03.16   BarCie initial version
+ */
+#ifndef BARCIE_INTERFACE_H
+#define BARCIE_INTERFACE_H
+
+
+// Every function that is exposed should be placed here
+#include "BARCIE_task_1.h"
+
+
+#endif
